52949ebcfd80d1fd239eebcaaa86cfd7dd4c80a7
grigorii goncharov <grigoriy85@gmail.com>
2025-07-14 18:03:40 +0300

    добавлены пути в модуле urls пакета catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
+from catalog.views import home, contacts
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', include('catalog.urls', namespace="catalog")),
+    #path('', include('catalog.urls', namespace="catalog")),
+    path('', home, name="home"),
+    path('', contacts, name="contacts"),
+
 ]
