604928a3ba6b8ca53f73b1f623c30d8180242ba4
grigorii goncharov <grigoriy85@gmail.com>
2025-07-14 20:31:31 +0300

    добавлен README

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
 def home(request):
     return render(request, template_name="home.html")
+
 
 # def contacts(request):
 #     return render(request, template_name="contacts.html")
