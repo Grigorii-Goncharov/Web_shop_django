41697db4e0bb1535e3d4d3e6ed4874ea3379d5f7
grigorii goncharov <grigoriy85@gmail.com>
2025-08-06 17:26:09 +0300

    создание модели blogost и применены миграции

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+# from django.urls import path
+# from catalog.apps import CatalogConfig
+# from .views import HomeView, ContactsView, ProductDetailView
+#
+# app_name = CatalogConfig.name
+#
+# urlpatterns = [
+#     path('', HomeView.as_view(), name="home"),  # главная страница
+#     path('contacts/', ContactsView.as_view(), name="contacts"),
+#     path('product/<int:pk>/', ProductDetailView.as_view(), name="product"),
+#
+# ]
