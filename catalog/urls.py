03d8900a0f8a9d946b0bb3ebf716ea5d2c52a25b
grigorii goncharov <grigoriy85@gmail.com>
2025-08-11 19:08:32 +0300

    добавлен раздел Создать продукт

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from .views import HomeView, ContactsView, ProductDetailView
+from .views import HomeView, ContactsView, ProductDetailView, ProductsCreateView
 
 app_name = CatalogConfig.name
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),  # главная страница
     path('contacts/', ContactsView.as_view(), name="contacts"),
     path('product/<int:pk>/', ProductDetailView.as_view(), name="product"),
+    path('product/create/', ProductsCreateView.as_view(), name="create"),
 
 ]
