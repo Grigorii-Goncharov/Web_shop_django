c9108b2113f12f75123a42c5a66fc4a4877e3ee0
grigorii goncharov <grigoriy85@gmail.com>
2025-08-11 18:12:27 +0300

    создан модуль forms в пакете catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from .models import Products, Category
 from django.views import View
-
+from .forms import ProductsForm
 
 class HomeView(ListView):
     model = Products
@@ -24,6 +25,26 @@ class ProductDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['category'] = Category.objects.all()  # добавляем категории в контекст
         return context
+
+
+class ProductsCreateView(CreateView):
+    model = Products
+    form_class = ProductsForm
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductsUpdateView(UpdateView):
+    model = Products
+    form_class = ProductsForm
+
+    def get_success_url(self):
+        return reverse('catalog:products_detail', kwargs = {'pk': self.object.pk})
+
+
+class ProductsDeleteView(DeleteView):
+    model = Products
+    success_url = reverse_lazy('catalog:products_list')
+
 
 
 # ФУНКЦИЯ ОТОБРАЖЕНИЯ И ОТПРАВКИ ФОРМЫ ЗАПРОСА
